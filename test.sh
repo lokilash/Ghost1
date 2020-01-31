cd0d381413d760d5e7e3d5eb085456fd07b61d27
root <root@localhost.localdomain>
2020-01-31 17:34:30 +0530

    command commit

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+a=`df -h |grep -vE '^Filesystem|tmpfs|cdrom' | grep "/var/backup" | awk '{print $5}' | sed 's/\%//g' `
+if [ $a -gt 0 ]; then
+  rsync -avhr /var/backup root@192.168.56.102:/var/ghost
+fi
+if [ $? -eq 0 ]; then
+  rm -rf /var/backup/*
+fi
+
+
